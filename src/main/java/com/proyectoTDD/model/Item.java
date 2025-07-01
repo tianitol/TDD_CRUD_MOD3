513ec5bac0aceb11991a486e95192241d0ca7f52
Nicole Alexandra Henriquez <ale.nicolehs@gmail.com>
2025-07-01 01:34:39 -0400

    REFACTOR_ItemTest [Se agrega lombok para eliminar todo el codigo repetitivo de getters, setters y constructores]

--- src/main/java/com/proyectoTDD/model/Item.java
+++ src/main/java/com/proyectoTDD/model/Item.java
@@ -1,56 +1,15 @@
 package com.proyectoTDD.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
     private int id;
     private String size;
     private String name;
     private int stock;
-
-    //Constructor
-
-    public Item() {
-    }
-
-    public Item(int id, String size, String name, int stock) {
-        this.id = id;
-        this.size = size;
-        this.name = name;
-        this.stock = stock;
-    }
-
-    //Getters
-
-    public int getId() {
-        return id;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    //Setters
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
 }
