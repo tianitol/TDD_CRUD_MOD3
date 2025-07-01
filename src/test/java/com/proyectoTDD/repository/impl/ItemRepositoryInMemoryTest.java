8505073c3735170c537f958f34832974045b0dba
Nicole Alexandra Henriquez <ale.nicolehs@gmail.com>
2025-07-01 02:00:02 -0400

    RED_testSaveAndFindById ItemRepositoryInMemoryTest

---
+++ src/test/java/com/proyectoTDD/repository/impl/ItemRepositoryInMemoryTest.java
@@ -0,0 +1,33 @@
+package com.proyectoTDD.repository.impl;
+
+import com.proyectoTDD.model.Item;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemRepositoryInMemoryTest {
+    private ItemRepositoryInMemory repository;
+
+    @BeforeEach
+    public void setUp() {
+        repository = new ItemRepositoryInMemory();
+    }
+
+    @Test
+    public void testSaveAndFindById() {
+
+        Item item = new Item(1, "m", "Plato", 10);
+
+        repository.save(item);
+        Optional<Item> result = repository.findById(1);
+
+        //Assert
+        assertTrue(result.isPresent());
+        assertEquals("Plato", result.get().getName());
+        assertEquals("m", result.get().getSize());
+        assertEquals(10, result.get().getStock());
+    }
+}
